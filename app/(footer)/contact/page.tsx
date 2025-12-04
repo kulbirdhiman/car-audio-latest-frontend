6a683d92c0456bb6d0f20b42c63dca9b570e7f23
Karan Dhiman <136183168+kulbirdhiman@users.noreply.github.com>
2025-12-04 01:24:39 -0800

    fix it

--- app/(footer)/contact/page.tsx
+++ app/(footer)/contact/page.tsx
@@ -35,4 +35,9 @@ const ContactPage = () => {
   );
 };
 
-export default ContactPage;
+export default ContactPage;                                                                                                        
+
+
+
+
+
