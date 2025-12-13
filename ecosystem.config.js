207148efcd55f908f04ea3db701811b7dac0e23d
karandhiman <karandhiman9877@gmail.com>
2025-12-13 05:31:08 -0800

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "nextjs-app",
+      script: "npm",
+      args: "start",
+      env: {
+        PORT: 3001,
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
